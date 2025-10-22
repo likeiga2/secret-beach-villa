70e08fc3761164908f1f54c8d64cef5d6812fd7f
likeiga2 <takuyaaoshima@gmail.com>
2025-10-22 21:36:10 +0900

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <div style={{ padding: "4rem 1rem", textAlign: "center" }}>
+      <h2>お問い合わせ</h2>
+      <p>ご質問やメディア受付などは、以下よりご連絡ください。</p>
+      <a href="mailto:info@secretbeachvilla.com" style={{ color: "#000" }}>
+        info@secretbeachvilla.com
+      </a>
+    </div>
+  );
+}
