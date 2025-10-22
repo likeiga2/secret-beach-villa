79df7e928c43efec84b7d456a71cc5c75204de54
likeiga2 <takuyaaoshima@gmail.com>
2025-10-22 17:10:05 +0900

    Create pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fafafa', fontSize: '2rem', fontWeight: 'bold', color: '#333' }}>
+      Welcome to Secret Beach Villa Fukutsu \u2728
+    </div>
+  );
+}
