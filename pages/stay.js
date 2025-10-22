ac75634deb235683c64a7ed4915b1bfc737ef341
likeiga2 <takuyaaoshima@gmail.com>
2025-10-22 21:34:48 +0900

    Create stay.js

---
+++ pages/stay.js
@@ -0,0 +1,14 @@
+export default function Stay() {
+  return (
+    <div style={{ padding: "4rem 1rem", textAlign: "center" }}>
+      <h2>ご宿泊・ご予約</h2>
+      <p>下記フォームからご予約いただけます。</p>
+      <iframe
+        src="https://YOURNAME.beds24.com/booking2.php"
+        width="100%"
+        height="800"
+        style={{ border: "none", marginTop: "2rem" }}
+      ></iframe>
+    </div>
+  );
+}
